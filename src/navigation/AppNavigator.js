1a83972012ef7b97da83628ee1623806400d563b
saike1119 <saike1119@gmail.com>
2019-01-06 08:12:14 +0900

    storeから値を取得できるようにメソッドを追加

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -61,4 +61,29 @@ const AppNavigator = createStackNavigator(
   }
 );
 
+const navigateOnce = getStateForAction => (action, state) => {
+  const { type, routeName } = action;
+  if (state && type === NavigationActions.NAVIGATE) {
+    if (routeName === state.routes[state.routes.length - 1].routeName) {
+      return null;
+    }
+  }
+  return getStateForAction(action, state);
+};
+
+export const getActiveRouteName = navigationState => {
+  if (!navigationState) {
+    return null;
+  }
+  const route = navigationState.routes[navigationState.index];
+  if (route.routes) {
+    return getActiveRouteName(route);
+  }
+  return route.routeName;
+};
+
+AppNavigator.router.getStateForAction = navigateOnce(
+  AppNavigator.router.getStateForAction
+);
+
 export default AppNavigator;
