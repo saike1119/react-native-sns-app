4f696b8eb08cd452aec8d647956ea89f980d3302
saike1119 <saike1119@gmail.com>
2019-01-06 07:50:38 +0900

    MainTabNavigator作成

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,63 +1,67 @@
-import React from 'react';
-import { Platform } from 'react-native';
+import { Constants } from 'expo';
+import { createStackNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+/* from app */
+import HomeScreen from 'app/src/screens/HomeScreen';
+import SearchScreen from 'app/src/screens/SearchScreen';
+import NotificationScreen from 'app/src/screens/NotificationScreen';
+import UserScreen from 'app/src/screens/UserScreen';
 import {
-  createStackNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation';
+  HomeTabIcon,
+  SearchTabIcon,
+  TakeTabIcon,
+  NotificationTabIcon,
+  MeTabIcon,
+  TabBar,
+} from 'app/src/components/Tab';
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+const createTabStack = (title, screen) =>
+  createStackNavigator({
+    [title]: { screen },
+  });
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-});
-
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
-};
-
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
-
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-};
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
-    />
-  ),
-};
-
-export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
-});
+export default createBottomTabNavigator(
+  {
+    HomeTabs: {
+      screen: createTabStack('HomeTabs', HomeScreen),
+      navigationOptions: () => ({
+        tabBarIcon: SearchTabIcon,
+      }),
+    },
+    TakeTab: {
+      screen: () => null,
+      navigationOptions: ({ navigation }) => ({
+        tabBarIcon: TakeTabIcon,
+        tabBarOnPress: () => {
+          navigation.push('TakeModal');
+        },
+      }),
+    },
+    NotificationTab: {
+      screen: createTabStack('NotificationTab', NotificationScreen),
+      navigationOptions: () => ({
+        tabBarIcon: NotificationTabIcon,
+      }),
+    },
+    MeTab: {
+      screen: createTabStack('MeTab', UserScreen),
+      navigationOptions: () => ({
+        tabBarIcon: MeTabIcon,
+      }),
+    },
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      activeTintColor: '#333',
+      inactiveTintColor: '#bbb',
+      style: {
+        backgroundColor: Constants.manifest.extra.backgroundColor,
+      },
+    },
+    tabBarComponent: TabBar,
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+);
