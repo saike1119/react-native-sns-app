f126d9d86d233c6e6a154d27d6385e14892e281d
saike1119 <saike1119@gmail.com>
2019-01-06 08:26:15 +0900

    navとscreens作成

---
+++ src/reducers/screens.js
@@ -0,0 +1,10 @@
+const initialState = null;
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case 'SCREEN_SET':
+      return payload.current;
+    default:
+      return state;
+  }
+};
