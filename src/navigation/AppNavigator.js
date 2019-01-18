bc8fdf336ce49be356e4d8be33e1ef7054db2dc7
saike1119 <saike1119@gmail.com>
2019-01-18 09:31:21 +0900

    AppNavigator修正

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -68,6 +68,11 @@ const navigateOnce = getStateForAction => (action, state) => {
       return null;
     }
   }
+  if (state && type === 'TAKEMODAL_CLOSE') {
+    const newRoutes = state.routes.filter(r => r.routeName !== 'TakeModal');
+    const newIndex = newRoutes.length - 1;
+    return getStateForAction(action, { index: newIndex, routes: newRoutes });
+  }
   return getStateForAction(action, state);
 };
 
